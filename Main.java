e38eddf8f127c8edf840e4281d15e28697179418
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-08-24 17:05:02 +0000

    Its important for a constructor to have parameters.

--- Main.java
+++ Main.java
@@ -14,3 +14,7 @@
 // Bicycle roverBike = new Bicycle(30, 0, 8);
 // a constructor is called by the new operator.
 // creates space in memory for the object and initializes its fields
+
+// You can use access modifiers in a constructor's declaration to control which other classes can call the constructor.
+
+// The compiler automatically provides a no-argument, default constructor for any class without constructors. This default constructor will call the no-argument constructor of the superclass. In this situation, the compiler will complain if the superclass doesn't have a no-argument constructor so you must verify that it does. If your class has no explicit superclass, then it has an implicit superclass of Object, which does have a no-argument constructor.
