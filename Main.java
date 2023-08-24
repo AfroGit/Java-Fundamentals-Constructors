cdb667c9445002f1234297e9ec88e1b12e232b15
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-08-24 16:55:55 +0000

    As with methods, the Java platform differentiates constructors on the basis of the number of arguments in the list and their types. You cannot write two constructors that have the same number and type of arguments for the same class, because the platform would not be able to tell them apart. Doing so causes a compile-time error.

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
-class Main {
+// class Main {
 //   public static void main(String[] args) {
 //     System.out.println("Hello world!");
 //   }
 // }
-/* */ 
-  public Bicycle(int startCadence, int startSpeed, int startGear) {
-    gear = startGear;
-    cadence = startCadence;
-    speed = startSpeed;
-}
+/* Constructor declarations look like method declarations—except that they use the name of the class and have no return type. For example, Bicycle has one constructor: */ 
+
+//   public Bicycle(int startCadence, int startSpeed, int startGear) {
+//     gear = startGear;
+//     cadence = startCadence;
+//     speed = startSpeed;
+// }
+
+// Bicycle roverBike = new Bicycle(30, 0, 8);
+// a constructor is called by the new operator.
+// creates space in memory for the object and initializes its fields
