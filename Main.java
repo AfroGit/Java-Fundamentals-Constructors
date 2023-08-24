79d967d3beb068460c3725ba4a4d85d27e32a4aa
AfroGit <17697323-AfroGit@users.noreply.replit.com>
2023-08-24 16:42:45 +0000

    A class contains constructors that are invoked to create objects from the class blueprint.

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+//   public static void main(String[] args) {
+//     System.out.println("Hello world!");
+//   }
+// }
+/* */ 
+  public Bicycle(int startCadence, int startSpeed, int startGear) {
+    gear = startGear;
+    cadence = startCadence;
+    speed = startSpeed;
 }
